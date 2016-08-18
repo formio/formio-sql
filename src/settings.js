ee7ee24819b99d90a896bf7190f365d4a0013401
Zack Urben <zackurben@gmail.com>
2016-08-18 13:49:07 -0500

    Adding pluck system to defaults to make it simpler to add new settings.

--- src/settings.js
+++ src/settings.js
@@ -3,29 +3,80 @@
 var _ = require('lodash');
 var defaults = require('../defaults.json');
 var debug = {
+  pluck: require('debug')('formio-sql:settings:pluck'),
   settings: require('debug')('formio-sql:settings')
 };
 
 // The required settings, which will stop the server from starting.
-var required = ['project', 'type', 'db.host'];
+var required = ['project', 'type', 'db.server'];
 
 module.exports = function() {
-  var settings = {
-    project: _.get(process, 'env.PROJECT'),
-    port: _.get(process, 'env.PORT') || _.get(defaults, 'port'),
-    type: _.get(process, 'env.TYPE') || _.get(defaults, 'type'),
-    db: {
-      server: _.get(process, 'env.DB_SERVER') || _.get(defaults, 'db.server'),
-      user: _.get(process, 'env.DB_USER') || _.get(defaults, 'db.user')
+  var settings = {};
+
+  /**
+   * Simple util to pluck settings from env or the defaults.
+   *
+   * NOTE: All vars in .env must be UPPERCASE.
+   *
+   * @param {String} first
+   *   The first path to search
+   * @param [String] second
+   *   The fallback path to search (if different than first, if false, no fallback will be searched)
+   */
+  var pluck = function(first, second) {
+    // One or the other is required - fail.
+    if (first === undefined && second === undefined) {
+      debug.pluck('Not given a first or second');
+      return undefined;
     }
+    else if (first === undefined && second !== undefined) {
+      debug.pluck('Params in the wrong order');
+      return undefined;
+    }
+
+    // If no second was given, default it to the first before mutation.
+    if (second === undefined) {
+      second = first;
+    }
+
+    // Mutate the first search path for env parsing.
+    first = 'env.' + first.toString().toUpperCase();
+
+    // Search the user defined settings.
+    var temp = _.get(settings, first);
+    debug.pluck(first + ': ' + temp);
+
+    // If fallback was disabled, return the original results.
+    if (second === false) {
+      debug.pluck('No fallback (' + first + ')');
+      return temp;
+    }
+
+    // If the original search was undefined (user), attempt to use the system defaults.
+    temp = temp !== undefined
+      ? temp
+      : _.get(defaults, second);
+
+    debug.pluck('Fallback (' + second + '): ' + temp);
+    return temp;
   };
 
+  // Build the settings with the plucked properties.
+  settings = {
+    project: pluck('project', false),
+    port: pluck('port'),
+    type: pluck('type'),
+    db: {
+      server: pluck('db_server', 'db.server'),
+      user: pluck('db_user', 'db.user')
+    }
+  };
   debug.settings(settings);
 
   // Iterate all required fields, and notify the user of all missing fields.
   var missing = [];
   required.forEach(function(setting) {
-    if (!_.has(settings, setting) || settings[setting] === undefined) {
+    if (!_.has(settings, setting) || _.get(settings, setting) === undefined) {
       missing.push(setting);
     }
   });
